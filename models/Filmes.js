a29afe91d2f70e511455f4c12635de4197400051
Matheus Ferrari Abrahão <101658952+MatheusFAbr@users.noreply.github.com>
2024-12-02 23:22:30 -0300

    Add files via upload

---
+++ models/Filmes.js
@@ -0,0 +1,25 @@
+import { Sequelize } from "sequelize";
+import connection from "../config/sequelize-config.js";
+
+const Filmes = connection.define("filmes", {
+  file: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  titulo: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  ano: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+  classificacao: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+Filmes.sync({ force: false });
+
+export default Filmes;
